33342f05e10064385b27db0daafb1e3d04f9ae42
pyschoopath <47376855+pyschoopath@users.noreply.github.com>
2019-02-06 00:59:51 -0800

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class EvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        
+        int num = reader.nextInt();
+
+        if(num % 2 == 0)
+            System.out.println(num + "  even");
+        else
+            System.out.println(num + "  odd");
+    }
+}
